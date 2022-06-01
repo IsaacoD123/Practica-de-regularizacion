d2a219d3ea19d15b7ba0018845f0c582db31065f
Isaac Ramírez <ramirezlimonesisaac@gmail.com>
2022-06-01 07:51:16 +0200

    agrego modelos Lasso y ridge

--- regularization.py
+++ regularization.py
@@ -17,3 +17,35 @@ if __name__=='__main__':
 
     print(X.shape)
     print(y.shape)
+
+    X_train,X_test,y_train,y_test=train_test_split(X,y,test_size=0.25)
+
+    modelLinear=LinearRegression().fit(X_train,y_train)
+    y_predict_linear=modelLinear.predict(X_test)
+
+    modelLasso=Lasso(alpha=0.02).fit(X_train,y_train)
+    y_predict_lasso=modelLasso.predict(X_test)
+
+    modelRidge=Ridge(alpha=1).fit(X_train,y_train)
+    y_predict_ridge=modelRidge.predict(X_test)
+
+    linear_loss=mean_squared_error(y_test,y_predict_linear)
+    print('linear Loss: ',linear_loss)
+
+    lasso_loss=mean_squared_error(y_test,y_predict_lasso)
+    print('Lasso loss: ', lasso_loss)
+
+    ridge_loss=mean_squared_error(y_test,y_predict_ridge)
+    print('Ridge Loss: ', ridge_loss)
+
+    print('='*32)
+    print('Coef Lasso: ' )
+    print(modelLasso.coef)
+
+    print('='*32)
+
+    print('Coef Ridge: ' )
+    print(modelridge.coef)
+
+
+
